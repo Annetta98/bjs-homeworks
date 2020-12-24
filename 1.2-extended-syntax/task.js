e3849cd70079eb6c1ed0246b74a629206fbecb74
Annetta98 <67956192+Annetta98@users.noreply.github.com>
2020-12-24 10:51:01 +0500

    Add files via upload

--- 1.2-extended-syntax/task.js
+++ 1.2-extended-syntax/task.js
@@ -1,9 +1,26 @@
-function getResult(a,b,c){
-    // код для задачи №1 писать здесь
-    // return x;
-}
+"use strict"
+function getResult(){
+    let a = 2;
+    let b = 4;
+    let c = 3;
+    let formula = (a*x** + b*x + c==0);
+    let D = b** - 4*a*c;
+    if (D < 0){
+      console.log [];
+    }
+    if (D == 0){
+        console.log [-b/2a];
+    }
+    if (D > 0){
+        console.log [-b+Math.sqrt(D)/2a];
+        console.log [-b-Math.sqrt(D)/2a];
+  }
+  return x
 
 function getAverageMark(marks){
+    let reting [];
+    if 
+
     // код для задачи №2 писать здесь
     // return averageMark;
 }
